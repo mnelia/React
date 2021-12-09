76734401c616e74a6c8528ce47502fdacf2af3e4
Matias <matiaseliagiusto@gmail.com>
2021-12-09 17:02:27 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Navbar from "./components/Navbar/Navbar"
 import './App.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ComponenteUseEffect from './components/ComponenteUseEffect';
 
 function App() {
+  const inicial = 1
+  const max = 10
   return (
     <div className="App">
       <Navbar />
       <ItemListContainer greeting="Hola soy Item list Container que vengo de App " />
+      <ComponenteUseEffect inicial={inicial} max={max} />
+
     </div>
   );
 }
---
+++ src/components/ComponenteUseEffect.js
@@ -0,0 +1,27 @@
+
+import { useState } from 'react'
+
+function ComponenteUseEffect({ max, inicial}) {
+    const [ contador, setContador ] = useState(inicial)
+
+    const handleSuma = () => {
+       contador < max ?  setContador(contador + 1) : alert ('compra maxima')
+    }
+
+    const handleResta = () => {
+        contador < inicial ?  setContador(contador - 1) : alert ('compra minima')
+    }
+
+
+
+    return (
+        <div>
+            <p>{contador}</p>
+            <button onClick={handleSuma}>Click +</button>
+            <button onClick={handleResta}>Click -</button>
+            
+        </div>
+    )
+}
+
+export default ComponenteUseEffect
